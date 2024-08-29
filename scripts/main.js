fb79d3ee95db320e6929b74d18d9041d2b6a1684
EBayego <edubayego@gmail.com>
2024-08-29 13:21:33 +0200

    Added Animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('section');
 
-    sections.forEach((section, index) => {
-        section.style.animationDelay = `${index * 0.5}s`;
+    // Agregar la clase "hidden" a todas las secciones inicialmente
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    const observerOptions = {
+        root: null, // El viewport
+        rootMargin: '0px',
+        threshold: 0.1 // El 10% de la sección debe ser visible para activarse
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                entry.target.classList.remove('hidden');
+                observer.unobserve(entry.target); // Dejar de observar una vez que la sección sea visible
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
     });
 });
